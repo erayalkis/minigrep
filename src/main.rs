b36c8429dba861815ef9a9515fff18cee3e82f45
erayalkis <erayalkis@hotmail.com>
2022-05-23 16:14:18 +0300

    Create Config::new method

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,19 @@ struct Config {
     filename: String
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let filename = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let query = args[1].clone();
+        let filename = args[2].clone();
 
-    Config { query, filename }
+        Config { query, filename }
+    }
 }
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
     let content = fs::read_to_string(config.filename).expect("Could not read file.");
     println!("{}", content);
