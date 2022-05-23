b70aa8042b150863bd08a520aacf9e9ee7898a89
erayalkis <erayalkis@hotmail.com>
2022-05-23 17:06:28 +0300

    Split logic into separate file

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,6 @@
 use std::env; // Import std::env to read Environment variables
-use std::fs; // Import std::fs to read and write files
 use std::process; // Import process to exit with code
-use std::error::Error;
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-
-        Ok(Config { query, filename })
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("{}", contents);
-
-    Ok(())
-}
+use minigrep::Config;
 
 
 fn main() {
@@ -40,7 +13,7 @@ fn main() {
     println!("Searching for {}", config.query);
     println!("Inside file {}", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = minigrep::run(config) {
         println!("Application error: {}", e);
         process::exit(1);
     }
