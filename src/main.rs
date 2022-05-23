208543ea3d1241b807866aa339a62990c58f8e3a
erayalkis <erayalkis@hotmail.com>
2022-05-23 16:10:20 +0300

    Add struct for handling config

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 use std::env; // Import std::env to read Environment variables
+use std::fs; // Import std::fs to read and write files
+
+struct Config {
+    query: String,
+    filename: String
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("Args {:?}", args);
+    let config = parse_config(&args);
+
+    let content = fs::read_to_string(config.filename).expect("Could not read file.");
+    println!("{}", content);
 }
