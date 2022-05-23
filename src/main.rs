58aba456aa9c01f64dc2c96cdfe742467f4fbff7
erayalkis <erayalkis@hotmail.com>
2022-05-23 17:32:10 +0300

    Write errors to stderr instead of stdout

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problems parsing arguments: {}", err);
+        eprintln!("Problems parsing arguments: {}", err);
         process::exit(1);
     });
 
@@ -14,7 +14,7 @@ fn main() {
     println!("Inside file {}", config.filename);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {}", e);
+        eprintln!("Application error: {}", e);
         process::exit(1);
     }
 }
