750e4f33aab1237f3a0695252631bc73a528241b
erayalkis <erayalkis@hotmail.com>
2022-05-23 16:21:11 +0300

    Add error handling with Result enum to Config::new

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::env; // Import std::env to read Environment variables
 use std::fs; // Import std::fs to read and write files
+use std::process; // Import process to exit with code
 
 struct Config {
     query: String,
@@ -7,22 +8,25 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
-            panic!("Not enough arguments");
+            return Err("Not enough arguments");
         }
 
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
 
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problems parsing arguments: {}", err);
+        process::exit(1);
+    });
 
     let content = fs::read_to_string(config.filename).expect("Could not read file.");
     println!("{}", content);
